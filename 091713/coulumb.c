a79ccc7d3947a89beeee54cfe82b78b06b1d48a2
Matias Altman <matiasa123@gmail.com>
2013-10-08 14:56:22 -0700

    Added file reading On windows doesn't work yet

--- 091713/coulumb.c
+++ 091713/coulumb.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv)
 {
-	if(argc < ARG_MIN)
+	if(argc <= ARG_MIN)
 	{
 		printf("Error: Requires More Argument\n");
 		return 1;
@@ -12,6 +12,30 @@ int main(int argc, char **argv)
 
 	int mass_1, mass_2;
 	float posn_1x, posn_1y, posn_2x, posn_2y;
+
+	if(argc == 2)
+	{
+		FILE *fr;
+
+		//printf("Argument 1: %s\n",argv[1]);
+		fr = fopen(argv[1], "r+");
+
+		printf("Opening %s\n", argv[1]);
+
+		rewind(fr);
+
+		fscanf(fr, "%d %f %f\n", mass_1, posn_1x, posn_1y);	
+		fscanf(fr, "%d %f %f", mass_2, posn_2x, posn_2y);	
+
+		fclose(fr);
+
+		printf("Mass1: %d Mass2: %d\n", mass_1, mass_2);
+		printf("(x_1, y_1): ( %d,%d)  ", posn_1x, posn_1y);
+		printf("(x_2, y_2): ( %d,%d)\n", posn_2x, posn_2y);
+	}
+	/**else if(argc == 6)
+	{
+	}**/
 
 	return 0;
 }
