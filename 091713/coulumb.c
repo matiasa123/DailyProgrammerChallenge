4fff49be71cc84aba35aa0e29d368a722b75932e
Matias Altman <matiasa123@gmail.com>
2013-10-27 22:29:20 -0700

    Added a function to compute the distance

--- 091713/coulumb.c
+++ 091713/coulumb.c
@@ -9,8 +9,16 @@ typedef struct particle
 	float mass;
 } Particle;
 
+static float distance(Particle *a, Particle *b)
+{
+	float dx,dy;
+	dx = a->x - b->x, dy = a->y - b->y;
+
+	return sqrtf((dx*dx) + (dy*dy));
+}
+
 int main(int argc, char **argv)
 {
-		
+
 	return 0;
 }
