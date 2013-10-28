c2a50e2d3d0db56a0ff2acb65977435e4f82bfae
Matias Altman <matiasa123@gmail.com>
2013-10-27 22:49:18 -0700

    Added code to take in input

--- 091713/coulumb.c
+++ 091713/coulumb.c
@@ -19,6 +19,20 @@ static float distance(Particle *a, Particle *b)
 
 int main(int argc, char **argv)
 {
+	Particle _p[2];
+	float distance;
+	int i;
+
+	for(i = 0; i < 2; i++)
+	{
+		scanf("%f %f %f", &_p[i].mass, &_p[i].x, &_p[i].y);
+	}
+
+
+	/**
+	printf("particle1 mass: %f particle1 x: %f particle1 y: %f\n", _p[0].mass, _p[0].x, _p[0].y);
+	printf("particle2 mass: %f particle2 x: %f particle2 y: %f\n", _p[1].mass, _p[1].x, _p[1].y);
+	**/
 
 	return 0;
 }
