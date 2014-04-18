ee8e263a9b3ff8eddfa3ce48b1b25d1ec7d1cb42
matiasa123 <matiasa123@gmail.com>
2014-04-18 15:24:35 -0700

    Wrote initial code to read in input

--- 041814/TicTacToe.c
+++ 041814/TicTacToe.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 
+#define WIDTH 3
+#define HEIGHT 3
+#define INAROW 3
+
 int main( int argc, char* argv[] )
 {
+	//Initialize Variables
+	int i, j;
+
+	//Take in Input
+	char grid[WIDTH][HEIGHT]; 
+	
+	for(i = 0; i < WIDTH; i++)
+	{
+		for(j = 0; j < HEIGHT; j++)
+		{
+			scanf("%c", &grid[i][j]);	
+		}
+		scanf("\n");
+	}
+
+	for(i = 0; i < WIDTH; i++)
+	{
+		for(j = 0; j < HEIGHT; j++)
+		{
+			printf("%c", grid[i][j]);	
+		}
+		printf("\n");
+	}
+
+	//PrintGrid(grid);	
+
 	return 0;
 }
+
+/**
+void PrintGrid(char grid[][])
+{
+	//Initialize Varialbes
+	int i, j;
+	
+	//Print out Grid
+	for(i = 0; i < WIDTH; i++)
+	{
+		for(j = 0; j < HEIGHT; j++)
+		{
+			printf("%c", grid[i][j]);	
+		}
+		printf("\n");
+	}
+}**/
