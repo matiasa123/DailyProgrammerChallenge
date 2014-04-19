ab148c1109f2881d045d0f792d23f8617a2c3c04
matiasa123 <matiasa123@gmail.com>
2014-04-18 18:30:46 -0700

    Added Next Move Functionality

--- 041814/TicTacToe.c
+++ 041814/TicTacToe.c
@@ -7,10 +7,10 @@
 void PrintGrid(char grid[HEIGHT][WIDTH]);
 int is0(char);
 int isX(char);
-int CheckRow(int RowNumber, char grid[HEIGHT][WIDTH]);
-int CheckCol(int ColNumber, char grid[HEIGHT][WIDTH]);
-int CheckDiagLR(int ColNumber, char grid[HEIGHT][WIDTH]);
-int CheckDiagRL(int ColNumber, char grid[HEIGHT][WIDTH]);
+int CheckRow(int RowNumber, char grid[HEIGHT][WIDTH], char Next);
+int CheckCol(int ColNumber, char grid[HEIGHT][WIDTH], char Next);
+int CheckDiagLR(int ColNumber, char grid[HEIGHT][WIDTH], char Next);
+int CheckDiagRL(int ColNumber, char grid[HEIGHT][WIDTH], char Next);
 
 
 int main( int argc, char* argv[] )
@@ -19,8 +19,10 @@ int main( int argc, char* argv[] )
 	int i, j;
 
 	//Take in Input
-	char grid[HEIGHT][WIDTH]; 
-	
+	char grid[HEIGHT][WIDTH], NextMove; 
+
+
+	scanf("%c",&NextMove);
 	for(i = 0; i < WIDTH; i++)
 	{
 		scanf("\n");
@@ -33,13 +35,14 @@ int main( int argc, char* argv[] )
 
 
 	printf("====\n");
+	printf("Next Move: %c\n", NextMove);
 	PrintGrid(grid);	
 
 	printf("====\n");
 	printf("Row Check\n");
 	for(i = 0; i < HEIGHT; i++)
 	{
-		printf("Row %d: %d\n", i, CheckRow(i, grid));
+		printf("Row %d: %d\n", i, CheckRow(i, grid, NextMove));
 	}
 
 
@@ -47,17 +50,17 @@ int main( int argc, char* argv[] )
 	printf("Column Check\n");
 	for(i = 0; i < WIDTH; i++)
 	{
-		printf("Column %d: %d\n", i, CheckCol(i, grid));
+		printf("Column %d: %d\n", i, CheckCol(i, grid, NextMove));
 	}
 	printf("====\n");
 	printf("Diagonal Check\n");
 	for(i = 0; i < WIDTH; i++)
 	{
-		printf("Column %d: %d\n", i, CheckDiagLR(i, grid));
+		printf("Column %d: %d\n", i, CheckDiagLR(i, grid, NextMove));
 	}
 	for(i = 0; i < WIDTH; i++)
 	{
-		printf("Column %d: %d\n", i, CheckDiagRL(i, grid));
+		printf("Column %d: %d\n", i, CheckDiagRL(i, grid, NextMove));
 	}
 
 	return 0;
@@ -73,7 +76,7 @@ int isX(char val)
 	return (val == 'x' || val == 'X');
 }
 
-int CheckRow(int RowNumber, char grid[HEIGHT][WIDTH])
+int CheckRow(int RowNumber, char grid[HEIGHT][WIDTH], char Next)
 {
 	int i, sum;
 
@@ -95,10 +98,21 @@ int CheckRow(int RowNumber, char grid[HEIGHT][WIDTH])
 		}
 	}
 	
-	return ((sum == INAROW)||((sum*(-1)) == INAROW));	
+	if((isX(Next)))	
+	{
+		return ((sum == INAROW));	
+	}
+	else if((is0(Next)))	
+	{
+		return ((sum*(-1)) == INAROW);	
+	}
+	else
+	{
+		return 0;
+	}
 }
 
-int CheckCol(int ColNumber, char grid[HEIGHT][WIDTH])
+int CheckCol(int ColNumber, char grid[HEIGHT][WIDTH], char Next)
 {
 	int i, sum;
 
@@ -120,10 +134,21 @@ int CheckCol(int ColNumber, char grid[HEIGHT][WIDTH])
 		}
 	}
 	
-	return ((sum == INAROW)||((sum*(-1)) == INAROW));	
+	if((isX(Next)))	
+	{
+		return ((sum == INAROW));	
+	}
+	else if((is0(Next)))	
+	{
+		return ((sum*(-1)) == INAROW);	
+	}
+	else
+	{
+		return 0;
+	}
 }
 
-int CheckDiagLR(int ColNumber, char grid[HEIGHT][WIDTH])
+int CheckDiagLR(int ColNumber, char grid[HEIGHT][WIDTH], char Next)
 {
 	int i, j, sum;
 
@@ -145,10 +170,21 @@ int CheckDiagLR(int ColNumber, char grid[HEIGHT][WIDTH])
 		}
 	}
 	
-	return ((sum == INAROW)||((sum*(-1)) == INAROW));	
+	if((isX(Next)))	
+	{
+		return ((sum == INAROW));	
+	}
+	else if((is0(Next)))	
+	{
+		return ((sum*(-1)) == INAROW);	
+	}
+	else
+	{
+		return 0;
+	}
 }
 
-int CheckDiagRL(int ColNumber, char grid[HEIGHT][WIDTH])
+int CheckDiagRL(int ColNumber, char grid[HEIGHT][WIDTH], char Next)
 {
 	int i, j, sum;
 
@@ -169,8 +205,19 @@ int CheckDiagRL(int ColNumber, char grid[HEIGHT][WIDTH])
 			sum--;
 		}
 	}
-	
-	return ((sum == INAROW)||((sum*(-1)) == INAROW));	
+
+	if((isX(Next)))	
+	{
+		return ((sum == INAROW));	
+	}
+	else if((is0(Next)))	
+	{
+		return ((sum*(-1)) == INAROW);	
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 void PrintGrid(char grid[HEIGHT][WIDTH])
