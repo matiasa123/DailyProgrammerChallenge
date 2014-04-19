2da6e7e7798aa37dd1f1cf15b86994c8778fbe67
matiasa123 <matiasa123@gmail.com>
2014-04-18 17:28:28 -0700

    Added a print grid function and Functions to check rows and columns

--- 041814/TicTacToe.c
+++ 041814/TicTacToe.c
@@ -4,50 +4,125 @@
 #define HEIGHT 3
 #define INAROW 3
 
+void PrintGrid(char grid[HEIGHT][WIDTH]);
+int is0(char);
+int isX(char);
+int CheckRow(int RowNumber, char grid[HEIGHT][WIDTH]);
+int CheckCol(int ColNumber, char grid[HEIGHT][WIDTH]);
+
+
 int main( int argc, char* argv[] )
 {
 	//Initialize Variables
 	int i, j;
 
 	//Take in Input
-	char grid[WIDTH][HEIGHT]; 
+	char grid[HEIGHT][WIDTH]; 
 	
 	for(i = 0; i < WIDTH; i++)
 	{
+		scanf("\n");
+		
 		for(j = 0; j < HEIGHT; j++)
 		{
 			scanf("%c", &grid[i][j]);	
 		}
-		scanf("\n");
 	}
 
+
+	printf("====\n");
+	PrintGrid(grid);	
+
+	printf("====\n");
+	printf("Row Check\n");
+	for(i = 0; i < HEIGHT; i++)
+	{
+		printf("Row %d: %d\n", i, CheckRow(i, grid));
+	}
+
+
+	printf("====\n");
+	printf("Column Check\n");
 	for(i = 0; i < WIDTH; i++)
 	{
-		for(j = 0; j < HEIGHT; j++)
-		{
-			printf("%c", grid[i][j]);	
-		}
-		printf("\n");
+		printf("Column %d: %d\n", i, CheckCol(i, grid));
 	}
-
-	//PrintGrid(grid);	
 
 	return 0;
 }
 
-/**
-void PrintGrid(char grid[][])
+int is0(char val)
+{
+	return ((val == '0') || (val == 'O') || (val == 'o'));
+}
+
+int isX(char val)
+{
+	return (val == 'x' || val == 'X');
+}
+
+int CheckRow(int RowNumber, char grid[HEIGHT][WIDTH])
+{
+	int i, sum;
+
+	if(RowNumber >= HEIGHT || RowNumber < 0)
+	{
+		return 0;
+	}
+
+	sum = 0;
+	for(i = 0; i < WIDTH; i++)
+	{
+		if(isX(grid[RowNumber][i]))
+		{
+			sum++;
+		}	
+		else if(is0(grid[RowNumber][i]))
+		{
+			sum--;
+		}
+	}
+	
+	return ((sum == INAROW)||((sum*(-1)) == INAROW));	
+}
+
+int CheckCol(int ColNumber, char grid[HEIGHT][WIDTH])
+{
+	int i, sum;
+
+	if(ColNumber >= WIDTH || ColNumber < 0)
+	{
+		return 0;
+	}
+
+	sum = 0;
+	for(i = 0; i < WIDTH; i++)
+	{
+		if(isX(grid[i][ColNumber]))
+		{
+			sum++;
+		}	
+		else if(is0(grid[i][ColNumber]))
+		{
+			sum--;
+		}
+	}
+	
+	return ((sum == INAROW)||((sum*(-1)) == INAROW));	
+}
+
+void PrintGrid(char grid[HEIGHT][WIDTH])
 {
 	//Initialize Varialbes
 	int i, j;
 	
 	//Print out Grid
-	for(i = 0; i < WIDTH; i++)
+	for(i = 0; i < HEIGHT; i++)
 	{
-		for(j = 0; j < HEIGHT; j++)
+		for(j = 0; j < WIDTH; j++)
 		{
 			printf("%c", grid[i][j]);	
 		}
 		printf("\n");
 	}
-}**/
+}
